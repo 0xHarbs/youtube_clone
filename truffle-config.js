783a1c934795a98edacc4ca76f0e20db76da8f1c
Harbs <jamesharbridge@outlook.com>
2022-03-14 12:47:04 +0000

    Updates before Heroku push

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,11 @@
 require('babel-register');
 require('babel-polyfill');
+require('dotenv').config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const private_keys = [
+  process.env.PRIVATE_KEY_0,
+  process.env.PRIVATE_KEY_1,
+]
 
 module.exports = {
   networks: {
@@ -7,6 +13,20 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider({
+        private_keys: process.env.PRIVATE_KEY_0,
+        providerOrUrl: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+        numberOfAddresses: 1,
+        mnemonic: process.env.SECRET_KEY
+      }
+      ),
+      network_id: 4,
+      gas: 6500000,
+      gasPrice: 10000000000,
+      confirmations: 2,
+      timeoutBlocks: 200,
     },
   },
   contracts_directory: './src/contracts/',
